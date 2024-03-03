914544d8ff9afa7df02d8572ab99a216a11c598a
Nima <nimafeli138384@gmail.com>
2024-03-03 11:00:29 +0330

    complete Warmupfile

--- Answers/40230112105/src/main/java/Warmup.java
+++ Answers/40230112105/src/main/java/Warmup.java
@@ -6,7 +6,12 @@ public class Warmup {
      * @return is the number th word of the sentence
      */
     public String wordFinder(String sentence, int number) {
-        return null;
+        String[] devided = sentence.split(" ");
+        if(number > sentence.length){
+            return ("Number = " + number + " is out Of Bound");
+        }else{
+            return devided[number-1];
+        }
     }
 
     /**
@@ -16,7 +21,23 @@ public class Warmup {
      * @return if searchForEven is true ? return the number of even numbers : return the number of odd numbers
      */
     public int oddEvenCounter(String number, boolean searchForEven) {
-        return -1;
+        int converted = Integer.valueOf(number);
+        int temp = converted;
+        int evenCounter=0, oddCounter=0;
+        while(temp != 0){
+            int last = temp % 10;
+            if(last % 2 == 0){
+                evenCounter++;
+            }else{
+                oddCounter++;
+            }
+            temp /= 10;
+        }
+        if(searchForEven){
+            return evenCounter;
+        }else{
+            return oddCounter;
+        }
     }
 
     /**
@@ -25,6 +46,14 @@ public class Warmup {
      * @return The word that is first in alphabet column
      */
     public String firstWord(String wordA, String wordB) {
-        return null;
+        int i = 0;
+        while(wordA.charAt(i) != "\0" && wordB.charAt(i) != "\0")
+        if(wordA.cahrAt(i) < wordB.charAt(i)){
+            return wordA;
+        }else if(wordA.charAt(i) > wordB.charAt(i)){
+            return wordB;
+        }else{
+            i++;
+        }
     }
 }
